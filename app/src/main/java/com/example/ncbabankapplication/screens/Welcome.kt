005a2bdcd56d717548aad325cb4cf94bc3c4fafa
erastusnzula <erastusnzula@gmail.com>
2025-03-29 11:25:34 +0300

    changes

--- app/src/main/java/com/example/ncbabankapplication/screens/Welcome.kt
+++ app/src/main/java/com/example/ncbabankapplication/screens/Welcome.kt
@@ -1,16 +1,34 @@
 package com.example.ncbabankapplication.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.ncbabankapplication.R
+import com.example.ncbabankapplication.ui.theme.NCBABANKAPPLICATIONTheme
 
 @Composable
 fun Welcome(navController: NavController) {
-    Scaffold {innerPadding->
+    Scaffold(
+        modifier = Modifier
+            .background(colorResource(R.color.white))
+            .padding(top = 28.dp, bottom = 28.dp),
+        topBar = {},
+        bottomBar = {},
+        containerColor = colorResource(R.color.white),
+        contentColor = colorResource(R.color.secondary),
+        contentWindowInsets = WindowInsets.systemBars
+    ) {innerPadding->
         Column(
             modifier = Modifier
                 .padding(innerPadding)
@@ -20,5 +38,13 @@ fun Welcome(navController: NavController) {
             )
 
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WelcomePreview(){
+    NCBABANKAPPLICATIONTheme {
+        Welcome(navController = rememberNavController())
     }
 }
